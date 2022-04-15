7bfa4e62ac8c47469e052ba1bc91c8de623a04db
kayshaou <kaysha.ou@gmail.com>
2022-04-15 17:34:51 +0800

    json project

---
+++ src/test/java/com/jakesmommy/lesson/json/JsonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jakesmommy.lesson.json;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
